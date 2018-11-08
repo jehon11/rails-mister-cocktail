de996ed148179b7a8f0fc4e77133a365b2dee73a
Jeremy Hon <jehon11@gmail.com>
2018-11-08 15:35:13 +0900

    finished basic functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :cocktails do
+    resources :doses, except: :delete
+  end
+  delete 'cocktail/doses/:id', to: 'doses#destroy', as: :delete_dose
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+from_web = JSON.parse(open(url).read)
+from_web['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+end
+
+Cocktail.create(name: 'Old Fashioned')
+Cocktail.create(name: 'Mojito')
+Cocktail.create(name: 'Gin and Tonic')
+Cocktail.create(name: 'Whisky Sour')
