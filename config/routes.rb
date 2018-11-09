b7c5e9ceafc7a05c9d04bf1e7b2f2f12d1d163b0
Jeremy Hon <jehon11@gmail.com>
2018-11-09 12:39:08 +0900

    added basic css and banner

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
   resources :cocktails do
     resources :doses, except: :delete
   end
-  delete 'cocktail/doses/:id', to: 'doses#destroy', as: :delete_dose
+  delete 'doses/:id', to: 'doses#destroy', as: :delete_dose
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
