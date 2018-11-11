847d108e7e3adf926c8c1c1aeb5dd216ebe5e7a6
Jeremy Hon <jehon11@gmail.com>
2018-11-11 17:41:51 +0900

    added stars to reviews

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,4 +4,25 @@ class Cocktail < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
+
+  def average_rating
+    sum = 0
+
+    reviews.each do |review|
+      sum += review.rating unless review.rating.nil?
+    end
+    if sum.fdiv(reviews.length).nan?
+      ''
+    else
+      sum.fdiv(reviews.length).fdiv(0.5).ceil * 0.5
+    end
+  end
+
+  def stars
+    average_rating == '' ? 0 : average_rating.floor
+  end
+
+  def half_star
+    average_rating == '' ? '' : !(average_rating % 1 == 0)
+  end
 end
